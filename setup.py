5158b04df6d29a7bb3534cb7b28540c10c92e60a
parolin <tarrus@hotmail.com>
2018-09-19 14:31:29 +0200

    Update setup.py
    pytest working.

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 import setuptools
 
-setup(name='awswafip',
-      version='0.1',
+setuptools.setup(name='awswafip',
+      version='0.1.1.3',
       description='Insert/Delete IP Address from a file in IP Address Conditions in AWS WAF',
       url='https://github.com/parolin/awswafip',
       author='Matheus L Parolin',
       author_email='mlparolin@gmail.com',
       license='GNU v3.0',
       packages=setuptools.find_packages(),
-      zip_safe=False)
+      zip_safe=False,
+      #use_2to3=True,
+      #long_description=open( 'README.rst' ).read(),
+      install_requires=[
+        'boto3',
+      ],
+      entry_points='''
+        [console_scripts]
+        awswafip=awswafip.updateips:cli
+      ''',
+)
